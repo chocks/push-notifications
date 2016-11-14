85965a29a15c18dd79f22701da4f13bcfe34e548
Chocks <chocks@outlook.com>
2016-11-13 21:55:12 -0600

    tutorial commits

--- app/sw.js
+++ app/sw.js
@@ -20,3 +20,44 @@
 /* eslint-env browser, serviceworker, es6 */
 
 'use strict';
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay push works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  const notificationPromise = self.registration.showNotification(title, options);
+  event.waitUntil(notificationPromise);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
+
+self.addEventListener('sync', function(event) {
+  console.log('[Service Worker] Sync Received.');
+  const title = 'Sync Codelab';
+  const options = {
+    body: 'Yay Sync works too.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  const notificationPromise = self.registration.showNotification(title, options);
+  event.waitUntil(notificationPromise);
+});
+
+
+
